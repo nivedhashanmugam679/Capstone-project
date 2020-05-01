f20d67a9213ff0f8697ab90c5225755a76031a16
nivedhashanmugam679 <35552026+nivedhashanmugam679@users.noreply.github.com>
2020-05-01 15:22:24 +0530

    Update collect-data.py

--- collect-data.py
+++ collect-data.py
@@ -97,8 +97,38 @@ while True:
              'two': len(os.listdir(directory+"/2")),
              'three': len(os.listdir(directory+"/3")),
              'four': len(os.listdir(directory+"/4")),
-             'five': len(os.listdir(directory+"/5"))}
-    
+             'five': len(os.listdir(directory+"/5"))
+             'six': len(os.listdir(directory+"/6")) 
+             'seven': len(os.listdir(directory+"/7"))
+             'eight': len(os.listdir(directory+"/8"))
+             'nine': len(os.listdir(directory+"/9"))
+             'A': len(os.listdir(directory+"/A"))
+             'B': len(os.listdir(directory+"/B"))
+             'C': len(os.listdir(directory+"/C"))
+             'D': len(os.listdir(directory+"/D"))
+             'E': len(os.listdir(directory+"/E"))
+             'F': len(os.listdir(directory+"/F"))
+             'G': len(os.listdir(directory+"/G"))
+             'H': len(os.listdir(directory+"/H"))
+             'I': len(os.listdir(directory+"/I"))
+             'J': len(os.listdir(directory+"/J"))
+             'K': len(os.listdir(directory+"/K"))
+             'L': len(os.listdir(directory+"/L"))
+             'M': len(os.listdir(directory+"/M"))
+             'N': len(os.listdir(directory+"/N"))
+             'O': len(os.listdir(directory+"/O"))
+             'P': len(os.listdir(directory+"/P"))
+             'Q': len(os.listdir(directory+"/Q"))
+             'R': len(os.listdir(directory+"/R"))
+             'S': len(os.listdir(directory+"/S"))
+             'T': len(os.listdir(directory+"/T"))
+             'U': len(os.listdir(directory+"/U"))
+             'V': len(os.listdir(directory+"/V"))
+             'W': len(os.listdir(directory+"/W"))
+             'X': len(os.listdir(directory+"/X"))
+             'Y': len(os.listdir(directory+"/Y"))
+             'Z': len(os.listdir(directory+"/Z"))
+
     # Printing the count in each set to the screen
     cv2.putText(frame, "MODE : "+mode, (10, 50), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
     cv2.putText(frame, "IMAGE COUNT", (10, 100), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
